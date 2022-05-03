58de7d88ff0dbfbc3b1fa1243d9d3e0f13e7839c
Filipe T. De Carvalho <fcarvalho@viceri.com.br>
2022-05-03 17:43:42 -0300

    page Home, conection prismic .

---
+++ src/pages/api/_lib/manageSubscription.ts
@@ -0,0 +1,57 @@
+import { fauna } from "../../../services/fauna";
+import { query as q } from "faunadb";
+import { stripe } from "../../../services/stripe";
+export async function saveSubscription(
+  subscriptionId: string,
+  customerId: string,
+  createAction = false
+) {
+  const userRef = await fauna.query(
+    q.Select(
+      "ref",
+      q.Get(q.Match(q.Index("user_by_stripe_customer_id"), customerId))
+    )
+  );
+
+  const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+
+  const subscriptionData = {
+    id: subscription.id,
+    userId: userRef,
+    status: subscription.status,
+    price_id: subscription.items.data[0].price.id,
+  };
+  if (createAction) {
+    fauna.query(
+      q.If(
+        q.Not(
+          q.Exists(
+            q.Match(
+              q.Index("subscription_by_user_id"),
+              q.Casefold(subscriptionData.userId)
+            )
+          )
+        ),
+        q.Create(q.Collection("subscriptions"), { data: subscriptionData }),
+        q.Get(
+          q.Match(
+            q.Index("subscription_by_user_id"),
+            q.Casefold(subscriptionData.userId)
+          )
+        )
+      )
+    );
+  } else {
+    await fauna.query(
+      q.Replace(
+        q.Select(
+          "ref",
+          q.Get(
+            q.Match(q.Index("subscription_by_user_id"), subscriptionData.userId)
+          )
+        ),
+        { data: subscriptionData }
+      )
+    );
+  }
+}
